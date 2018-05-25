2b9f857bd06d9ce8e7e651f9ed4d73ed6b7f840b
Matthew Barber <quitesimplymatt@gmail.com>
2018-05-25 23:23:43 +0100

    fixed toggle

--- toggle.js
+++ toggle.js
@@ -3,14 +3,14 @@ $('#toggle').click(() => {
     var text = $('#toggle');
     var body = $('body');
 
-    if (body.hasClass('--light')) {
-        body.removeClass('--light');
-        body.addClass('--dark');
-        text.html("Too edgy?");
-    }
-    else {
+    if (body.hasClass('--dark')) {
         body.removeClass('--dark');
         body.addClass('--light');
         text.html("Too bright?");
+    }
+    else {
+        body.removeClass('--light');
+        body.addClass('--dark');
+        text.html("Too edgy?");
     }
 });
